860446cb5e279dd4d3e54788643693bd62ebf8ff
Peter Souter <petems@users.noreply.github.com>
2025-09-01 04:26:16 +0100

    test: dedupe request in security headers test

--- test/securityHeaders.test.js
+++ test/securityHeaders.test.js
@@ -2,19 +2,19 @@ const request = require('supertest');
 
 describe('Security headers', () => {
   let app;
+  let res;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     process.env.NODE_ENV = 'test';
     app = require('../app');
+    res = await request(app).get('/');
   });
 
-  it('does not expose X-Powered-By header', async () => {
-    const res = await request(app).get('/');
+  it('does not expose X-Powered-By header', () => {
     expect(res.headers['x-powered-by']).toBeUndefined();
   });
 
-  it('sets key security headers via Helmet', async () => {
-    const res = await request(app).get('/');
+  it('sets key security headers via Helmet', () => {
     expect(res.headers['x-content-type-options']).toBe('nosniff');
     expect(res.headers['x-frame-options']).toBeDefined();
     expect(res.headers['content-security-policy']).toBeDefined();
